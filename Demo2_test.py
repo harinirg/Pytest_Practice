45d23428e825913e7b5cd5c9a4135c156ae28349
Harini R G <harinirg26@gmail.com>
2026-06-08 17:11:06 +0530

    Demo2Marker is committed

---
+++ Demo2_test.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_sample_one():
+    print("Hai")
+def test_sample1():
+    a=10
+    b=10
+    assert a==b
+def test_sample2():
+    a=5
+    b=10
+    assert a<b
+def test_sample3():
+    a="arun"
+    b="aruns"
+    assert a._eq_(b)
