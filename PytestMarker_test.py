1c6c29571986d178c1b9d21c858970f400e4a486
Harini R G <harinirg26@gmail.com>
2026-06-08 17:15:35 +0530

    PytestMarker is committed

---
+++ PytestMarker_test.py
@@ -0,0 +1,20 @@
+import pytest
+@pytest.mark.printstatement
+def test_sample_one():
+    print("Hai")
+@pytest.mark.xfail(reason="xfail")
+def test_sample1():
+    a=10
+    b=9
+    assert a==b
+@pytest.mark.lessernum
+def test_sample2():
+    a=5
+    b=10
+    assert a<b
+#@pytest.mark.skip(reason="No need")
+@pytest.mark.nameequal
+def test_sample3():
+    a="arun"
+    b="aruns"
+    assert a._eq_(b)
