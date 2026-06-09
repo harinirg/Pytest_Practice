ea5510548bf80841c50b724692f97e34bc45b8b6
Harini R G <harinirg26@gmail.com>
2026-06-09 23:00:13 +0530

    altered the path of file

--- read_config.py
+++ read_config.py
@@ -2,5 +2,5 @@ from configparser import ConfigParser
 
 def get_config(category,key):
     config=ConfigParser()
-    config.read("./config.ini")
+    config.read("config.ini")
     return config.get(category,key)
