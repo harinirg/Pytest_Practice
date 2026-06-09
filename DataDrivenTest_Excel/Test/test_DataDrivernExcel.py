bb43f97fc31e8dab38ff1fbaff2bcedbe5b448ae
Harini R G <harinirg26@gmail.com>
2026-06-09 22:41:41 +0530

    report added

--- DataDrivenTest_Excel/Test/test_DataDrivernExcel.py
+++ DataDrivenTest_Excel/Test/test_DataDrivernExcel.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-@pytest.mark.parametrize("username,password", excelReader.get_data("ExcelFiles/LoginData.xlsx", "login"))
+@pytest.mark.parametrize("username,password", excelReader.get_data("ExcelFiles/LoginData.xlsx","login"))
 class TestLogin1:
     def test_validlogin1(self, username, password):
         self.driver = webdriver.Chrome()
