bdf710a67995aae055ecd57f822921aa68af4e5a
Harini R G <harinirg26@gmail.com>
2026-06-09 07:01:24 +0530

    Two parametrize test is committed

--- TwoParametrize_test.py
+++ TwoParametrize_test.py
@@ -1,17 +1,16 @@
 import pytest
+import time
 from selenium import webdriver
-@pytest.mark.parametrize("input_browser", ["chrome", "edge"])
-@pytest.mark.parametrize("input_url", ["https://www.flipkart.com/", "https://www.amazon.com/"])
-def test_url(input_browser, input_url):
-    if input_browser == "chrome":
-        options = webdriver.ChromeOptions()
-        options.add_argument("--headless=new")
-        driver = webdriver.Chrome(options=options)
-    elif input_browser == "edge":
-        options = webdriver.EdgeOptions()
-        options.add_argument("--headless=new")
-        driver = webdriver.Edge(options=options)
-    driver.get(input_url)
-    print(f"Browser: {input_browser}")
-    print(f"Title: {driver.title}")
-    driver.quit()
+from selenium.webdriver.common.by import By
+@pytest.mark.parametrize('browser',['chrome','firefox'])
+@pytest.mark.parametrize('input_url',['https://www.flipkart.com/','https://www.amazon.in/'])
+def test_url(browser,input_url):
+    if browser == "chrome" :
+        web_driver = webdriver.Chrome()
+    if browser == "firefox" :
+        web_driver = webdriver.Firefox()
+    web_driver.maximize_window()
+    web_driver.get(input_url)
+    print(web_driver.title)
+    time.sleep(5)
+    web_driver.close()
